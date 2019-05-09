2f6589c435fa5289399bd19ed2b424f7d314589d
NikolaiZarudnev <nikolaizarudnrv@gmail.com>
2019-05-10 00:52:08 +0700

    Create empty window

--- src/main.c
+++ src/main.c
@@ -1,10 +1,12 @@
-#include "life.h"
+//#include "life.h"
 
-#include <stdio.h>
-#include <unistd.h>
+#include <gtk/gtk.h>
+//#include <stdio.h>
+//#include <unistd.h>
 
-int main()
+int main(int argc, char* argv[])
 {
+    /*
     unsigned char field[FIELD_SIZE][FIELD_SIZE];
 
     unsigned char* figure;
@@ -17,6 +19,20 @@ int main()
     }
 
     game_of_life(field, figure, nlines, nrows);
+    */
+    GtkWidget* window;
+
+    gtk_init(&argc, &argv);
+
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_default_size(GTK_WINDOW(window), 800, 600);
+    gtk_window_set_title(GTK_WINDOW(window), "Game of Life");
+
+    g_signal_connect(
+            G_OBJECT(window), "destroy", G_CALLBACK(gtk_main_quit), NULL);
+
+    gtk_widget_show_all(window);
+    gtk_main();
 
     return 0;
 }
